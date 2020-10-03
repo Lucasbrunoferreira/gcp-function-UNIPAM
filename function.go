cf25d8529e66df7404fd9f3edee8f8aeb92fe981
Lucas Bruno <lucasbrunoferreira@gmail.com>
2020-10-03 15:04:56 -0300

    Initial connection for firestore

--- function.go
+++ function.go
@@ -5,7 +5,10 @@ import (
   "fmt"
   "html"
   "net/http"
+
+  "cloud.google.com/go/firestore"
 )
+
 
 func CreateCars(w http.ResponseWriter, r *http.Request) {
 
@@ -17,10 +20,25 @@ func CreateCars(w http.ResponseWriter, r *http.Request) {
     Brand string `json:"brand"`
   }
 
+  ctx := context.Background()
+
 	if err := json.NewDecoder(r.Body).Decode(&car); err != nil {
 		fmt.Fprint(w, "Body inválido!")
 		return
   }
+
+  client, err := firestore.NewClient(ctx, "projectID")
+  if err != nil {
+    fmt.Fprint(w, "Erro ao conectar firestore.")
+  }
+
+  wr, err := ny.Create(ctx, car)
+
+  if err != nil {
+    fmt.Fprint(w, "Erro ao salvar o carro.")
+  }
+
+  fmt.Println(wr)
 
 	fmt.Fprint(w, html.EscapeString({car.Plaque, car.Color, car.Price, car.CarModel, car.Brand}))
 }
